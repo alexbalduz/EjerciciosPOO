e6fd41c3d7db188b4fc36511c01b707c9ef5d1a3
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-08 23:28:17 +0100

    constructor libro creado con todos los atributos

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,14 @@
 class Libro(object):
     def __init__(self):
-        self.titulo=""
-        self.editor=""
-        self.numpaginas=""
-        self.año=""
-        self.autor=""
+        self.titulo = ""
+        self.editor = ""
+        self.numpaginas = ""
+        self.año = ""
+        self.autor = ""
+
+    def libro(self, titulo, editor, numpaginas, año, autor):
+        self.titulo = titulo
+        self.editor = editor
+        self.numpaginas = numpaginas
+        self.año = año
+        self.autor = autor
