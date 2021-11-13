d67a8feb268def0806e76b3ad213cac231cb0a21
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-13 12:56:36 +0100

    metodos set

--- Ejercicio 4/CuentaBancaria.py
+++ Ejercicio 4/CuentaBancaria.py
@@ -5,9 +5,27 @@ class CuentaBancaria(object):
         self.fecha_apertura = ''
         self.numero_cuenta = 0
         self.saldo = 0
-#Definimos constructor
+
+    #Definimos constructor
     def cuentaBancaria(self, ID, titular, fecha_apertura, numero_cuenta):
         self.ID = ID
         self.titular = titular
         self.fecha_apertura = fecha_apertura
         self.numero_cuenta = numero_cuenta
+
+    #Método set
+    def setID(self, ID):
+        self.ID = ID
+
+    def settitular(self, titular):
+        self.titular = titular
+
+    def setfecha_apertura(self, fecha_apertura):
+        self.fecha_apertura = fecha_apertura
+
+    def setnumero_cuenta(self, numero_cuenta):
+        self.numero_cuenta = numero_cuenta
+
+    def setsaldo(self, saldo):
+        self.saldo = saldo
+
