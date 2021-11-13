fb3e71954f628eeec202bac8f75c2314420aad1c
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-13 12:55:15 +0100

    atributos

---
+++ Ejercicio 4/CuentaBancaria.py
@@ -0,0 +1,8 @@
+class CuentaBancaria(object):
+    def __init__(self):
+        self.ID = ''
+        self.titular = ''
+        self.fecha_apertura = ''
+        self.numero_cuenta = 0
+        self.saldo = 0
+
