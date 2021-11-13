cd27944f48a9bee55caf3ea0744c3f8a84fa042d
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-13 12:56:00 +0100

    constructor

--- Ejercicio 4/CuentaBancaria.py
+++ Ejercicio 4/CuentaBancaria.py
@@ -5,4 +5,9 @@ class CuentaBancaria(object):
         self.fecha_apertura = ''
         self.numero_cuenta = 0
         self.saldo = 0
-
+#Definimos constructor
+    def cuentaBancaria(self, ID, titular, fecha_apertura, numero_cuenta):
+        self.ID = ID
+        self.titular = titular
+        self.fecha_apertura = fecha_apertura
+        self.numero_cuenta = numero_cuenta
