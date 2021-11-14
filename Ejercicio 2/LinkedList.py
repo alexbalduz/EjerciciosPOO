442ec5f93668a53302844e996b6073ffdc59b333
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-14 22:17:49 +0100

    tabulaciones

--- Ejercicio 2/LinkedList.py
+++ Ejercicio 2/LinkedList.py
@@ -4,35 +4,35 @@ class LinkedList:
 		#inicializo la cabecera nula
 		self.head = None
 
-    def insertar(self, nodo_nuevo):
-    	#compruebo si la cabecera es nula o no
-		if self.head:
-			#cogemos el último nodo
-			last_node = self.head
-			while last_node != None:
-				last_node = last_node.next
+        def insertar(self, nodo_nuevo):
+            #compruebo si la cabecera es nula o no
+            if self.head:
+                #cogemos el último nodo
+                last_node = self.head
+                while last_node != None:
+                    last_node = last_node.next
 
-			#asignamos el nuevo nodo al siguiente punto del ultimo nodo
-			last_node.next = nodo_nuevo
+                #asignamos el nuevo nodo al siguiente punto del ultimo nodo
+                last_node.next = nodo_nuevo
 
-		else:
-			#cabecera esta vacía
-			#asignamos nodo a la cabecera
-			self.head = nodo_nuevo
+            else:
+                #cabecera esta vacía
+                #asignamos nodo a la cabecera
+                self.head = nodo_nuevo
 
-    def mostrar(self):
-    	#variable para iterar
-		temp_node = self.head
+        def mostrar(self):
+            #variable para iterar
+            temp_node = self.head
 
-		#iteramos hasta que llegamos al final de la lista
-		while temp_node != None:
-			#printamos el nodo data
-			print(temp_node.data, end='->')
+            #iteramos hasta que llegamos al final de la lista
+            while temp_node != None:
+                #printamos el nodo data
+                print(temp_node.data, end='->')
 
-			#nos movemos al siguiente nodo
-			temp_node = temp_node.next
+                #nos movemos al siguiente nodo
+                temp_node = temp_node.next
 
-		print('Null')
+            print('Null')
 
 #creamos un main para probarlo
 if __name__ == '__main__':
--- Ejercicio 2/LinkedListEnlazada.py
+++ Ejercicio 2/LinkedListEnlazada.py
@@ -4,41 +4,41 @@ class LinkedList:
 		#inicializo la cabecera nula
 		self.head = None
 
-    def insertar(self, nodo_nuevo):
-    		#compruebo si la cabecera es nula o no
-		if self.head:
-			#cogemos el último nodo
-			last_node = self.head
-			while last_node.next != None:
-				last_node = last_node.next
+        def insertar(self, nodo_nuevo):
+                #compruebo si la cabecera es nula o no
+            if self.head:
+                #cogemos el último nodo
+                last_node = self.head
+                while last_node.next != None:
+                    last_node = last_node.next
 
-			#asignamos el ultimo nodo al anterior del nuevo nodo
-			nodo_nuevo.prev = last_node
+                #asignamos el ultimo nodo al anterior del nuevo nodo
+                nodo_nuevo.prev = last_node
 
-			#asignamos el nuevo nodo al siguiente del ultimo nodo
-			last_node.next = nodo_nuevo
+                #asignamos el nuevo nodo al siguiente del ultimo nodo
+                last_node.next = nodo_nuevo
 
-    def mostrar(self):
+        def mostrar(self):
 
-		#printamos la data en orden normal
-		print("Orden normal: ", end='')
+            #printamos la data en orden normal
+            print("Orden normal: ", end='')
 
-		temp_node = self.head
-		while temp_node != None:
-			print(temp_node.data, end=' ')
-			temp_node = temp_node.next
-		print()
+            temp_node = self.head
+            while temp_node != None:
+                print(temp_node.data, end=' ')
+                temp_node = temp_node.next
+            print()
 
-		#printamos la data en orden inverso
-		print("Orden inverso: ", end='')
+            #printamos la data en orden inverso
+            print("Orden inverso: ", end='')
 
-		#cogemos el ultimo nodo
-		last_node = self.head
-		while last_node.next != None:
-			last_node = last_node.next
+            #cogemos el ultimo nodo
+            last_node = self.head
+            while last_node.next != None:
+                last_node = last_node.next
 
-		temp_node = last_node
-		while temp_node != None:
-			print(temp_node.data, end=' ')
-			temp_node = temp_node.prev
-		print()
+            temp_node = last_node
+            while temp_node != None:
+                print(temp_node.data, end=' ')
+                temp_node = temp_node.prev
+            print()
